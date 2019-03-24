722e0becf333076f25c038b81ce4f4ad678d8e2a
Libert Sin <majesticer13@gmail.com>
2019-03-24 12:26:50 +0000

    Upload New File

---
+++ 01_기본형, 출력함수, 상수, 변수개념/002_num.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+main()
+{
+	printf("10 : %d \n", 20);
+	printf("8  : %o \n", 20);
+	printf("16 : %x \n", 20);
+	printf("0.0: %f \n", 20.0);
+	printf("0e0: %e \n", 20.0);
+	printf("fuck : %g \n", 20.0);
+}
